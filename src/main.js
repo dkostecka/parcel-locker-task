94b813f5f117463a5e204d00861089f72234fcca
doma <domciakostecka@gmail.com>
2022-01-30 22:45:22 +0100

    add validation functions, add error message

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 containerDiv = document.getElementById('container');
+isCodeNumberInvalid = true;
+isPhoneNumberInvalid = true;
 
 function startDelivery() {
     document.getElementById("startDeliveryButton").remove();
     createDeliveryForm();
 }
+
 function createDeliveryForm() {
     let formDelivery = this.containerDiv.appendChild(document.createElement('form'));
     formDelivery.name = 'deliveryForm';
@@ -16,46 +19,73 @@ function createDeliveryForm() {
     createSuccesDeliveryButton(formDelivery);
 
 }
+
 function createPhoneNumber(formDelivery) {
     let phoneNumberDiv = formDelivery.appendChild(document.createElement('div'));
     phoneNumberDiv.className = 'delivery-form__div';
+
     let phoneNumberLabel = document.createElement('label');
     phoneNumberLabel.className = 'delivery-form__label'
     phoneNumberLabel.innerText = "Numer telefonu:";
     phoneNumberDiv.appendChild(phoneNumberLabel);
+
     let phoneNumberInput = phoneNumberDiv.appendChild(document.createElement('input'));
     phoneNumberInput.type = 'text';
     phoneNumberInput.name = 'phoneNumber';
+
+    let phoneNumberError = phoneNumberDiv.appendChild(document.createElement('span'));
+    phoneNumberError.id = 'phoneNumberError';
+    phoneNumberError.className = "delivery-form__span--error";
+    phoneNumberError.innerHTML = '<p>Numer telefonu musi składać się z 9 cyfr (0-9).</p>';
+
+    phoneNumberInput.onchange = function () { validatePhoneNumber() };
 }
 
 function createCodeNumber(formDelivery) {
     let codeNumberDiv = formDelivery.appendChild(document.createElement('div'));
+
     let codeNumberLabel = document.createElement('label');
     codeNumberLabel.className = 'delivery-form__label'
     codeNumberLabel.innerText = "Kod odbioru:";
+
     codeNumberDiv.appendChild(codeNumberLabel);
     codeNumberDiv.className = 'delivery-form__div';
+
     let codeNumberInput = codeNumberDiv.appendChild(document.createElement('input'));
     codeNumberInput.type = 'text';
     codeNumberInput.name = 'codeNumber';
+
+    let codeNumberError = codeNumberDiv.appendChild(document.createElement('span'));
+    codeNumberError.id = 'codeNumberError';
+    codeNumberError.className = "delivery-form__span--error";
+    codeNumberError.innerHTML = '<p>Kod odbioru musi składać się z 4 cyfr (0-9).</p>';
+
+    codeNumberInput.onchange = function () { validateCodeNumber() };
 }
+
 function createSuccesDeliveryButton(formDelivery) {
     let succesDeliveryButton = formDelivery.appendChild(document.createElement('button'));
     succesDeliveryButton.innerHTML = "Odbierz paczkę";
     succesDeliveryButton.id = 'succesDeliveryButton'
     succesDeliveryButton.className = 'container__button';
+    succesDeliveryButton.disabled = true;
     succesDeliveryButton.onclick = function () {
         succesDelivery()
     };
 }
+
 function succesDelivery() {
     document.getElementById("deliveryForm").remove();
+
     let summaryDiv = this.containerDiv.appendChild(document.createElement('div'));
     summaryDiv.id = "summary";
+
     let h3 = summaryDiv.appendChild(document.createElement('h3'));
     h3.innerHTML = "Paczka odebrana!"
+
     let p = summaryDiv.appendChild(document.createElement('p'));
     p.innerHTML = "Zrobiłeś to w czasie 10 sekund! Czy możemy zrobić dla ciebie coś jeszcze?";
+
     createEndDeliveryButton(summaryDiv);
     createNextDeliveryButton(summaryDiv);
 }
@@ -82,12 +112,14 @@ function createNextDeliveryButton(summaryDiv) {
 
 function nextDelivery() {
     document.getElementById("summary").remove();
-    
+    setFormValidity(true);
     createDeliveryForm();
 };
 
 function endDelivery() {
     document.getElementById("summary").remove();
+    setFormValidity(true);
+    
     let startDeliveryButton = this.containerDiv.appendChild(document.createElement('button'));
     startDeliveryButton.innerHTML = "Odbierz paczkę";
     startDeliveryButton.id = 'startDeliveryButton'
@@ -95,4 +127,55 @@ function endDelivery() {
     startDeliveryButton.onclick = function () {
         startDelivery()
     };
+}
+
+function validatePhoneNumber() {
+    let phoneNumber = document.forms["deliveryForm"]["phoneNumber"].value;
+    if (
+        phoneNumber == null ||
+        phoneNumber == "" ||
+        phoneNumber.length != 9 ||
+        !/^[0-9]+$/.test(phoneNumber)
+    ) {
+        this.isPhoneNumberInvalid = true;
+        document.getElementById("phoneNumberError").style.visibility = "visible";
+    } else {
+        this.isPhoneNumberInvalid = false;
+        document.getElementById("phoneNumberError").style.visibility = "hidden";
+    }
+
+    validateForm();
+}
+
+function validateCodeNumber() {
+    let codeNumber = document.forms["deliveryForm"]["codeNumber"].value;
+    if (
+        codeNumber == null ||
+        codeNumber === "" ||
+        codeNumber.length != 4 ||
+        !/^[0-9]+$/.test(codeNumber)
+    ) {
+        this.isCodeNumberInvalid = true;
+        document.getElementById("codeNumberError").style.visibility = "visible";
+    } else {
+        this.isCodeNumberInvalid = false;
+        document.getElementById("codeNumberError").style.visibility = "hidden";
+    }
+
+    validateForm();
+}
+
+function validateForm() {
+    if (this.isCodeNumberInvalid || this.isPhoneNumberInvalid) {
+        document.getElementById("succesDeliveryButton").disabled = true;
+        console.log("invalid");
+    } else if (!this.isCodeNumberInvalid && !this.isPhoneNumberInvalid) {
+        document.getElementById("succesDeliveryButton").disabled = false;
+        console.log("valid");
+    }
+}
+
+function setFormValidity(isInvalid) {
+    this.isPhoneNumberInvalid = isInvalid;
+    this.isCodeNumberInvalid = isInvalid;
 }

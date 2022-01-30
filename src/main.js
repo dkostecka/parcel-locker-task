921eb507e4c2abb7a90e27679282fb3493081e70
doma <domciakostecka@gmail.com>
2022-01-30 21:24:00 +0100

    add create form function, add styling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,45 @@
 function startDelivery() {
     document.getElementById("startDeliveryButton").remove();
+    createDeliveryForm();
+}
+function createDeliveryForm() {
+    let containerDiv = document.getElementById('container');
+
+    let formDelivery = containerDiv.appendChild(document.createElement('formDelivery'));
+    formDelivery.name = 'deliveryForm';
+    formDelivery.id = 'deliveryForm';
+    formDelivery.className = 'delivery-formDelivery';
+
+    let phoneNumberDiv = formDelivery.appendChild(document.createElement('div'));
+    phoneNumberDiv.className = 'delivery-form__div';
+    let phoneNumberLabel = document.createElement('label');
+    phoneNumberLabel.className = 'delivery-form__label'
+    phoneNumberLabel.innerText = "Numer telefonu:";
+    phoneNumberDiv.appendChild(phoneNumberLabel);
+    let phoneNumberInput = phoneNumberDiv.appendChild(document.createElement('input'));
+    phoneNumberInput.type = 'text';
+    phoneNumberInput.name = 'phoneNumber';
+
+    let codeNumberDiv = formDelivery.appendChild(document.createElement('div'));
+    let codeNumberLabel = document.createElement('label');
+    codeNumberLabel.className = 'delivery-form__label'
+    codeNumberLabel.innerText = "Kod odbioru:";
+    codeNumberDiv.appendChild(codeNumberLabel);
+    codeNumberDiv.className = 'delivery-form__div';
+    let codeNumberInput = codeNumberDiv.appendChild(document.createElement('input'));
+    codeNumberInput.type = 'text';
+    codeNumberInput.name = 'codeNumber';
+
+    formDelivery.appendChild(document.createElement("br"));
+
+    let succesDeliveryButton = formDelivery.appendChild(document.createElement('button'));
+    succesDeliveryButton.innerHTML = "Odbierz paczkę";
+    succesDeliveryButton.id = 'succesDeliveryButton'
+    succesDeliveryButton.className = 'container__button';
+    succesDeliveryButton.onclick = function () {
+        succesDelivery()
+    };
+}
+function succesDelivery() {
+    document.getElementById("deliveryForm").remove();
 }
